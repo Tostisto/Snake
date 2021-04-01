87e25b3a19e446b16fe79048501e9860747b0a45
Tostisto <contact@tostisto.com>
2021-04-01 19:00:07 +0200

    Add gestures

--- script.js
+++ script.js
@@ -178,5 +178,53 @@ function keypush(event) {
     if (event.key == 'ArrowRight' || event.key == 'd') {
         moveright();
     }
-
 }
+
+canvas.addEventListener("touchstart", startTouch, false);
+canvas.addEventListener("touchmove", moveTouch, false);
+
+var init_x = null;
+var init_y = null;
+
+function startTouch(event) {
+    init_x = event.touches[0].clientX;
+    init_y = event.touches[0].clientY;
+};
+
+function moveTouch(event) {
+    if (init_x === null) {
+        return 0;
+    }
+
+    if (init_y === null) {
+        return 0;
+    }
+
+    var curr_x = event.touches[0].clientX;
+    var curr_y = event.touches[0].clientY;
+
+    var control_x = init_x - curr_x;
+    var control_y = init_y - curr_y;
+
+    if (Math.abs(control_x) > Math.abs(control_y)) {
+        if (control_x < 0) {
+            moveright();
+        }
+        else {
+            moveleft();
+        }
+    }
+    else {
+        if (control_y < 0) {
+            movedown();
+        }
+        else {
+            moveup();
+        }
+    }
+
+    init_x = null;
+    init_y = null;
+
+    event.preventDefault();
+};
